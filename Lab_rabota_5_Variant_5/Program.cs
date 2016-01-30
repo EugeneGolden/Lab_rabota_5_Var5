668db79990938a52930b7af7af5303f9bf4f7c29
Eugene <yeug@gmail.com>
2016-01-30 19:40:52 +0200

    push

--- Lab_rabota_5_Variant_5/Program.cs
+++ Lab_rabota_5_Variant_5/Program.cs
@@ -12,23 +12,23 @@ namespace DeletingCharacters
         //Написать метод, входными параметрами которого являются две строки.
         //Метод должен в первой заданной строке удалить каждый символ, принадлежащий и второй заданной строке.
         //При решении не использовать готовые строковые функции
-        public void DeleteCharacter(string first, string second, out string firstString)
+        public void DeleteCharacter(ref string firstString, string secondString)
         {
             StringBuilder sb = new StringBuilder();
             HashSet<char> secondStringInSet = new HashSet<char>();
 
             //Добавим символы второй строки в HashSet
-            for (int i = 0; i < second.Length; i++)
+            for (int i = 0; i < secondString.Length; i++)
             {
-                secondStringInSet.Add(second[i]);
+                secondStringInSet.Add(secondString[i]);
             }
 
             //Добавим в StringBuilder только те символы, которых нет во второй строке, но есть в первой
-            for (int i = 0; i < first.Length; i++)
+            for (int i = 0; i < firstString.Length; i++)
             {
-                if (!secondStringInSet.Contains(first[i]))
+                if (!secondStringInSet.Contains(firstString[i]))
                 {
-                    sb.Append(first[i]);
+                    sb.Append(firstString[i]);
                 }
             }
 
@@ -43,10 +43,9 @@ namespace DeletingCharacters
             Console.WriteLine("Вторая строка: " + secondString);
 
             StringProcessing searchDelete = new StringProcessing();
-            searchDelete.DeleteCharacter(firstString, secondString, out firstString);
+            searchDelete.DeleteCharacter(ref firstString, secondString);
 
-            Console.WriteLine("Первая строка " + firstString);
-            Console.WriteLine("Результат после анализа: " + firstString);
+            Console.WriteLine("Первая строка после анализа: " + firstString);
 
             Console.ReadKey();
         }
